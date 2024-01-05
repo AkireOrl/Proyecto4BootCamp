ea3e3721daad68c8f3aaed2be7ad011d09042f5f
erika <om.erikaorlando@gmail.com>
2024-01-05 15:40:01 +0100

    Añadido modelo Design y cambio de imagen de diagrama

--- src/models/Design.ts
+++ src/models/Design.ts
@@ -1,6 +1,29 @@
-import { Entity } from "typeorm"
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from "typeorm"
+import { Artist } from "./Artist";
 
 @Entity()
-export class Design {
+export class Design extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    artist_id!: number;
+
+    @Column()
+    style!: string;
+
+    @Column()
+    picture!: string;
+
+    @Column()
+    created_at!: Date
+
+    @Column()
+    updated_at!: Date
+
+    @ManyToOne(() => Artist, (artist) => artist.users)
+    @JoinColumn ({name: "artist_id"})
+    artist!: Artist;
+
 
 }
